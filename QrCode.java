ae78740b4930e28e7323535abd5325b4f59fc994
Sidnei Lopes Ribeiro <59072337+geosidnei@users.noreply.github.com>
2020-08-02 15:46:02 -0300

    Add files via upload
    Here are the main files from this project.

---
+++ QrCode.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package qrcode;
+
+import javax.swing.JFrame;
+/**
+ *
+ * @author USER
+ */
+
+public class QrCode {
+
+    /**
+     * @param args the command line arguments
+     */
+  
+    
+    
+// public static void main(String[] args) {
+ // JFrame codes
+    
+public class bookInf {     
+    JFrame bookInf = new JFrame();
+    String bookInfTitulo;
+
+        public bookInf() {
+            this.bookInfTitulo = "Gera Qr Code para livros";
+        }
+       
+}
+    
+    }
+ 
+    
+
